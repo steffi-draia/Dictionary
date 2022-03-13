2927ee7aeecb97b16107b421c50e122a2952f632
steffi-draia <stefania.draia@yahoo.com>
2022-03-13 11:32:55 +0100

    update

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ var dictionary = [];
 
 function addWord() {
   var input = document.getElementById('word').value;
-	var showMessage = document.getElementById('statusInput');
-
+  var showMessage = document.getElementById('statusInput');
   if (dictionary.includes(input)) {
-  	showMessage.innerHTML = "This word already exists";
+    showMessage.innerHTML = "This word already exists";
   } else {
-  	var newWord = dictionary.push(input);
+    var newWord = dictionary.push(input);
     showMessage.innerHTML = "Added in the dictionary :)";
   }
 }
 
 function searchWord() {
-	var input = document.getElementById('word').value;
+  var input = document.getElementById('word').value;
   var showMessage = document.getElementById('statusInput');
   if (dictionary.includes(input)) {
-  	showMessage.colour = "red";
-  	showMessage.innerHTML = "This word already exists";
+    showMessage.colour = "red";
+    showMessage.innerHTML = "This word already exists";
   } else {
-  	showMessage.innerHTML = "You can add this word in the dictionary. Simply click on the button Add word :)";
+    showMessage.innerHTML = "You can add this word in the dictionary. Simply click on the button Add word :)";
   }
 }
